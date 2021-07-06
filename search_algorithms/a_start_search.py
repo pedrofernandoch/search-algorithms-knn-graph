d9e969b7fe4ad90ede7c2e1f9e836eb22b4e42cf
Pedro Christofoletti <pedrofernandoch@gmail.com>
2021-07-06 17:51:47 -0300

    Implementing a_star_search algorithm

--- search_algorithms/a_start_search.py
+++ search_algorithms/a_start_search.py
@@ -1,3 +1,21 @@
-def aStarSearch(graph, startVertex, endVertex):
-    print('aStarSearch')
+import numpy as np
+from queue import PriorityQueue
+
+def aStarSearch(graph, startVertex, endVertex, vertexArray):
+    visited = [False] * len(graph)
+    pq = PriorityQueue()
+    pq.put((0, startVertex))
+    path = []
+    while pq.empty() == False:
+        u = pq.get()[1]
+        path.append(u)
+        if u == endVertex:
+            return path
+        for v, c in graph[u]:
+            if visited[v] == False:
+                visited[v] = True
+                point1 = np.array(vertexArray[v])
+                point2 = np.array(vertexArray[endVertex])
+                euclideanDistance = np.linalg.norm(point1 - point2)
+                pq.put((c + euclideanDistance, v))
     return None

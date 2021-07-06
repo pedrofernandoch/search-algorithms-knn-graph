42a6fe12b8d5b9c0b4a0f28a99f3b2f629f4686e
Pedro Christofoletti <pedrofernandoch@gmail.com>
2021-07-06 15:59:10 -0300

    implementing  best_first_search

--- search_algorithms/best_first_search.py
+++ search_algorithms/best_first_search.py
@@ -1,3 +1,17 @@
+from queue import PriorityQueue
+
 def bestFirstSearch(graph, startVertex, endVertex):
-    print('bestFirstSearch')
+    visited = [False] * len(graph)
+    pq = PriorityQueue()
+    pq.put((0, startVertex))
+    path = []
+    while pq.empty() == False:
+        u = pq.get()[1]
+        path.append(u)
+        if u == endVertex:
+            return path
+        for v, c in graph[u]:
+            if visited[v] == False:
+                visited[v] = True
+                pq.put((c, v))
     return None

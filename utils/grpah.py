73e939b1499045d12c1e05a371d187714656d8f8
Pedro Christofoletti <pedrofernandoch@gmail.com>
2021-07-06 15:30:51 -0300

    Renaming lib and adding print graph function

--- utils/grpah.py
+++ utils/grpah.py
@@ -1,4 +1,6 @@
+from operator import length_hint
 import matplotlib.pyplot as plt
+from numpy import string_
 from utils.colors import bcolors
 
 def plotGraphWithPath(graph, dotsArray, path):
@@ -7,6 +9,16 @@ def plotGraphWithPath(graph, dotsArray, path):
         for y in range(len(dotsArray)):
             if x in path and y in path:
                 plt.plot([dotsArray[x][0], dotsArray[y][0]], [dotsArray[x][1], dotsArray[y][1]], 'bo-')
-            elif graph[x][y] == 1:
+            elif graph[x][y]:
                 plt.plot([dotsArray[x][0], dotsArray[y][0]], [dotsArray[x][1], dotsArray[y][1]], 'ro-')
     plt.show()
+
+def printGraphConnectivity(graph):
+    comma = ', '
+    rightSquareBracket = ' ]'
+    for i in range(len(graph)):
+        print(f'{i}: [ ', end='')
+        length = len(graph[i])
+        for j in range(length):
+            print(f'{graph[i][j][0]}{comma if j != (length -1) else rightSquareBracket}', end='')
+        print()

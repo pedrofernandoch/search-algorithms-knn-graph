a1185f8d226d25563823a8149d364b1665bf9a7f
Pedro Christofoletti <pedrofernandoch@gmail.com>
2021-07-06 20:43:39 -0300

    Moduling

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
-from operator import le
-import time
 import getpass
 import copy
 import numpy as np
 from sklearn.neighbors import kneighbors_graph
-from search_algorithms.breadth_first_search import breadthFirstSearch
-from search_algorithms.depth_first_search import depthFirstSearch
-from search_algorithms.best_first_search import bestFirstSearch
-from search_algorithms.a_search import aSearch
-from search_algorithms.a_star_search import aStarSearch
-from utils.graph import plotGraphWithPath, printGraphConnectivity
+from utils.graph import printGraphConnectivity, findPathAndInfo, plotGraphWithPath
 from utils.colors import bcolors
-
-# Search Algorithms
-search_algorithms = {
-    1: breadthFirstSearch,
-    2: depthFirstSearch,
-    3: bestFirstSearch,
-    4: aSearch,
-    5: aStarSearch
-}
 
 # Getting OS username
 try:
@@ -28,13 +12,13 @@ except Exception:
     username = 'usuário'
 print(f'{bcolors.HEADER}\nSaudações {username}!\n{bcolors.ENDC}')
 print('====================================================================================================')
-print(f'{bcolors.UNDERLINE}Este programa permite a criação de um grafo knn com o intuito de encontrar o caminho \n'+
-'entre dois vértices utilizando de diferentes tipos de algoritmos de busca. Além disso, é possivel\n'+
-'gerar a visualização gráfica do grafo com o caminho encontrado.')
+print(f'{bcolors.UNDERLINE}Este programa permite a criação de um grafo knn com o intuito de encontrar o caminho entre dois\n'+
+'vértices utilizando de diferentes tipos de algoritmos de busca. Além disso, é possivel gerar a\n'+
+'visualização gráfica do grafo com o caminho encontrado.')
 print('====================================================================================================\n')
 
 # Getting number of vertices and number of neighbors
-print(f'{bcolors.BOLD}Primeiro, vamos criar um grafo knn!\n')
+print(f'{bcolors.BOLD}Primeiro, vamos criar um grafo knn!{bcolors.ENDC}\n')
 while True:
     try:
         print(f'{bcolors.WARNING}AVISO: quanto maior o número de vértices e vizinhos maior será o tempo de execução e uso de memória. '+
@@ -68,11 +52,20 @@ for vertex in range(len(knn_graph)):
             knn_graph[vertex][neighbour] = -1
     knn_graph[vertex] = list(filter(lambda v: v != -1, knn_graph[vertex]))
 
-#printGraphConnectivity(knn_graph)
 print(f'Grafo knn criado com {bcolors.OKGREEN}Sucesso{bcolors.ENDC}!\n')
 
+while True: # Ask to show connections
+    shouldPlot = input(f'Deseja visualizar as conexões de cada vértice? (s/n): ')
+    if shouldPlot == 's' or shouldPlot == 'S':
+        printGraphConnectivity(knn_graph)
+        break
+    elif shouldPlot == 'n' or shouldPlot == 'N':
+        break
+    else:
+        print(f'{bcolors.FAIL}\nERRO: digite "s" para sim ou "n" para não.\n{bcolors.ENDC}')
+
 # Getting two input vertices
-print(f'{bcolors.BOLD}Agora vamos tentar encontrar o caminho entre dois vértices!\n')
+print(f'{bcolors.BOLD}\nAgora vamos tentar encontrar o caminho entre dois vértices!{bcolors.ENDC}\n')
 while True:
     try:
         startVertex = int(input(f'{bcolors.OKBLUE}Vértice inicial: {bcolors.ENDC}'))
@@ -89,26 +82,15 @@ print(f'\n{bcolors.BOLD}Agora escolha qual algoritmo para realizar a busca (núm
 while True:
     try:
         print(f'{bcolors.OKBLUE}1- Busca em largura')
-        print('2- Busca em profundidade')
-        print('3- Busca best first')
-        print('4- A')
-        print('5- A*')
+        print('2- Busca best first')
+        print('3- Busca A')
+        print('4- Busca A*')
+        print('5- Busca em profundidade')
         print(f'6- Todos os anteriores\n')
         option = int(input(f'Opção: {bcolors.ENDC}'))
         if option > 0 and option < 6:
-            print(f'\n{bcolors.OKCYAN}Procurando caminho...{bcolors.ENDC}')
-            start = time.time()
-            path = search_algorithms[option](knn_graph, startVertex, endVertex, vertexArray)
-            stop = time.time()
-            if path: # Path found
-                print(f'\nCaminho {bcolors.OKGREEN}encontrado{bcolors.ENDC}!\n')
-                print(f'{bcolors.HEADER}Tamanho do caminho: {bcolors.ENDC}{len(path)}')
-                print(f'{bcolors.HEADER}Tempo de execução: {bcolors.ENDC}{stop - start} segundos')
-                print(f'{bcolors.HEADER}Caminho encontrado: \n{bcolors.ENDC}')
-                print(f'{bcolors.OKCYAN}INICIO {bcolors.ENDC}-> ', end='')
-                for vertex in path:
-                    print(f'{vertex} -> ', end='')
-                print(f'{bcolors.OKCYAN}FIM{bcolors.ENDC}\n')
+            path = findPathAndInfo(option, knn_graph, startVertex, endVertex, vertexArray)
+            if path:
                 while True: # Plotting path
                     shouldPlot = input(f'Deseja visualizar o grafo com o caminho encontrado? (s/n): ')
                     if shouldPlot == 's' or shouldPlot == 'S':
@@ -118,11 +100,21 @@ while True:
                         break
                     else:
                         print(f'{bcolors.FAIL}\nERRO: digite "s" para sim ou "n" para não.\n{bcolors.ENDC}')
-            else: # Path not found
-                print(f'\nCaminho {bcolors.FAIL}não encontrado{bcolors.ENDC}.')
             break
         elif option == 6:
-            pass
+            for i in range(5):
+                path = findPathAndInfo(i+1, knn_graph, startVertex, endVertex, vertexArray)
+                if path:
+                    while True: # Plotting path
+                        shouldPlot = input(f'Deseja visualizar o grafo com o caminho encontrado? (s/n): ')
+                        if shouldPlot == 's' or shouldPlot == 'S':
+                            plotGraphWithPath(knn_graph_copy, vertexArray, path)
+                            break
+                        elif shouldPlot == 'n' or shouldPlot == 'N':
+                            break
+                        else:
+                            print(f'{bcolors.FAIL}\nERRO: digite "s" para sim ou "n" para não.\n{bcolors.ENDC}')
+            break
         else:
             print(f'{bcolors.FAIL}\nERRO: escolha uma das opções de 1 a 6, ou 0 para sair.\n{bcolors.ENDC}')
     except Exception:
--- utils/graph.py
+++ utils/graph.py
@@ -1,5 +1,20 @@
+import time
 import matplotlib.pyplot as plt
+from search_algorithms.breadth_first_search import breadthFirstSearch
+from search_algorithms.depth_first_search import depthFirstSearch
+from search_algorithms.best_first_search import bestFirstSearch
+from search_algorithms.a_search import aSearch
+from search_algorithms.a_star_search import aStarSearch
 from utils.colors import bcolors
+
+# Search Algorithms
+search_algorithms = {
+    1: {'func': breadthFirstSearch, 'title': 'Busca em largura'},
+    2: {'func': bestFirstSearch, 'title': 'Busca best first'},
+    3: {'func': aSearch, 'title': 'Busca A'},
+    4: {'func': aStarSearch, 'title': 'Busca A*'},
+    5: {'func': depthFirstSearch, 'title': 'Busca em profundidade'},
+}
 
 def plotGraphWithPath(graph, dotsArray, path):
     print(f'{bcolors.OKCYAN}\nPlotando grafo knn...{bcolors.ENDC}')
@@ -15,8 +30,29 @@ def printGraphConnectivity(graph):
     comma = ', '
     rightSquareBracket = ' ]'
     for i in range(len(graph)):
-        print(f'{i}: [ ', end='')
+        print(f'Vértice {i} está conectado com os vértices: [ ', end='')
         length = len(graph[i])
         for j in range(length):
             print(f'{graph[i][j][0]}{comma if j != (length -1) else rightSquareBracket}', end='')
         print()
+
+def findPathAndInfo(option, knn_graph, startVertex, endVertex, vertexArray):
+    title = search_algorithms[option]['title']
+    print(f'\n{bcolors.OKCYAN}Procurando caminho usando {title}...{bcolors.ENDC}')
+    start = time.time()
+    path = search_algorithms[option]['func'](knn_graph, startVertex, endVertex, vertexArray)
+    stop = time.time()
+    execTime = stop - start
+    if path: # Path found
+        print(f'\nCaminho {bcolors.OKGREEN}encontrado{bcolors.ENDC}!\n')
+        print(f'{bcolors.HEADER}Tamanho do caminho: {bcolors.ENDC}{len(path)}')
+        print(f'{bcolors.HEADER}Tempo de execução: {bcolors.ENDC}{execTime} segundos')
+        print(f'{bcolors.HEADER}Caminho encontrado: \n{bcolors.ENDC}')
+        print(f'{bcolors.OKCYAN}INICIO {bcolors.ENDC}-> ', end='')
+        for vertex in path:
+            print(f'{vertex} -> ', end='')
+        print(f'{bcolors.OKCYAN}FIM{bcolors.ENDC}\n')
+        return path
+    else: # Path not found
+        print(f'\nCaminho {bcolors.FAIL}não encontrado{bcolors.ENDC}.')
+        return None
